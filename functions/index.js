ee1e40cd2e8dd752252149d6948b8f3dffe96699
Phumrapee Limpianchop <git@rayriffy.com>
2018-10-01 21:48:38 +0700

    :recycle: Refactoring

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,5 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
 exports.api = functions.https.onRequest((req, res) => {
   var current_year = (new Date()).getFullYear();
 
@@ -14,45 +7,53 @@ exports.api = functions.https.onRequest((req, res) => {
 
   var check = new Date('03/21/' + current_year + ' 00:45:00 UTC+0700');
 
-  if (check - now <= 0) {
+  if (check - now <= 0)
     current_year++;
-  }
 
   let TargetDate = '03/21/' + current_year + ' 00:45:00 UTC+0700';
 
   let countDownDate = new Date(TargetDate).getTime();
   let distance = countDownDate - now;
-  
+
   var DaysInMonth = [
     31, 29, 31,
     30, 31, 30,
     31, 31, 30,
     31, 30, 31
   ];
-  
+
   function isLeap(Year) {
     return (Year % 4 === 0 && Year % 100 !== 0) || (Year % 400 === 0);
   }
-  
+
   function getDaysAndMonths(days) {
     let daysLeft = days;
     let thisMonth = (new Date()).getMonth();
     let monthsPassed = 0;
-    while ( daysLeft > DaysInMonth[thisMonth] ) {
-      let days = ( isLeap((new Date()).getFullYear()) && thisMonth == 1 ) ? DaysInMonth[thisMonth] + 1: DaysInMonth[thisMonth];
-      thisMonth = ( thisMonth + 1 >= 12 ) ? 0 : thisMonth + 1;
+    while (daysLeft > DaysInMonth[thisMonth]) {
+      let days = (isLeap((new Date()).getFullYear()) && thisMonth == 1) ? DaysInMonth[thisMonth] + 1 : DaysInMonth[thisMonth];
+      thisMonth = (thisMonth + 1 >= 12) ? 0 : thisMonth + 1;
       daysLeft = daysLeft - days;
       monthsPassed++;
     }
-    return {day: daysLeft, month: monthsPassed};
+    return {
+      day: daysLeft,
+      month: monthsPassed
+    };
   }
-  
-  let process = getDaysAndMonths(distance / (1000 * 60 * 60 *24));
-  
+
+  let process = getDaysAndMonths(distance / (1000 * 60 * 60 * 24));
+
   let months = Math.floor(process.month);
   let days = Math.floor(process.day);
   let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  res.status(200).json({ month: months, day: days, hour: hours, minute: minutes, second: seconds });
+  res.status(200).json({
+    month: months,
+    day: days,
+    hour: hours,
+    minute: minutes,
+    second: seconds
+  });
 })
